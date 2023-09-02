65805c00a86d119e091e1585139fd922a404cfaa
md sharffuddin <106474548+sharfuddin1986@users.noreply.github.com>
2023-09-02 14:52:32 +0300

    Create variables.tf

---
+++ Assignment2/variables.tf
@@ -0,0 +1,11 @@
+variable "access_key" {
+  description = "AWS access key"
+  type        = string
+  # default     = "YOUR_ACCESS_KEY" # You can set a default value if needed
+}
+
+variable "secret_key" {
+  description = "AKIAS2GMZPF5EY6MJY5G"
+  type        = string
+  # default     = "2V+XN4Syn3odjt7vhJtVphkHsjqPOlYA/kp34dnv" # You can set a default value if needed
+}
