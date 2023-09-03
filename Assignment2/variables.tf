7ff23eaeb95cd7d6460da52bdc6c43ca87617472
md sharffuddin <106474548+sharfuddin1986@users.noreply.github.com>
2023-09-03 08:13:20 +0300

    Update variables.tf

--- Assignment2/variables.tf
+++ Assignment2/variables.tf
@@ -1,11 +1,28 @@
-variable "access_key" {
-  description = "AWS access key"
-  type        = string
-  # default     = "YOUR_ACCESS_KEY" # You can set a default value if needed
+variable "aws_region" {
+  description = "AWS region for the EC2 instance and Elastic IP"
+  default     = "us-east-1"
 }
 
-variable "secret_key" {
-  description = "AKIAS2GMZPF5EY6MJY5G"
-  type        = string
-  # default     = "2V+XN4Syn3odjt7vhJtVphkHsjqPOlYA/kp34dnv" # You can set a default value if needed
+variable "access_key" {
+        description = "AKIAS2GMZPF5EY6MJY5G"
 }
+variable "secret_key" {
+        description = "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"
+}
+
+variable "ami_id" {
+  description = "AMI ID for the EC2 instance"
+  default     = "ami-053b0d53c279acc90"
+}
+
+variable "instance_type" {
+  description = "Instance type for the EC2 instance"
+  default     = "t2.micro"
+}
+
+variable "subnet_id" {
+  description = "Subnet ID where the EC2 instance will be launched"
+  default     = "subnet-0b39a527e8661296f"
+}
+
+
