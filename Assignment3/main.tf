ac7a4475d4a2b252cf8b74bab38208441040edab
md sharffuddin <106474548+sharfuddin1986@users.noreply.github.com>
2023-09-06 15:06:12 +0300

    Update main.tf

--- Assignment3/main.tf
+++ Assignment3/main.tf
@@ -1,3 +1,5 @@
+US-east-1 region main.tf file
+
 provider "aws" {
     alias = "NV"
     access_key = "Acess_key"
@@ -15,4 +17,26 @@ resource "aws_instance" "assignment-3-1" {
     Name = "hello-virginia"
         }
 }
+
+
+US-east-2 region main.tf file
+
+provider "aws" {
+    alias = "NV"
+    access_key = "AKIAS2GMZPF5COAOPF6L"
+    secret_key = "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"
+    region = "us-east-2"
+}
+
+resource "aws_instance" "assignment-3-2" {
+    provider = aws.NV
+    ami = "ami-024e6efaf93d85776"
+    count = "2"
+    instance_type = "t2.micro"
+    key_name = "ohio"
+    tags = {
+    Name = "hello-ohio"
+        }
+}
+
 
