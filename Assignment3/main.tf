41eb25224e9e2b9459bfeedfead5c51ce38cf78a
md sharffuddin <106474548+sharfuddin1986@users.noreply.github.com>
2023-09-06 14:30:18 +0300

    Update main.tf

--- Assignment3/main.tf
+++ Assignment3/main.tf
@@ -1,1 +1,18 @@
+provider "aws" {
+    alias = "NV"
+    access_key = "Acess_key"
+    secret_key = "Secret_key"
+    region = "us-east-1"
+}
+
+resource "aws_instance" "assignment-3-1" {
+    provider = aws.NV
+    ami = "ami-053b0d53c279acc90"
+    count = "2"
+    instance_type = "t2.micro"
+    key_name = "NV"
+    tags = {
+    Name = "hello-virginia"
+        }
+}
 
