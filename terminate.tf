007acd483882d57dc6fd0db551251e7d79b949ff
Md Sharfuddin <106474548+sharfuddin1986@users.noreply.github.com>
2024-11-28 11:39:04 +0300

    Update terminate.tf

--- terminate.tf
+++ terminate.tf
@@ -1,2 +1,10 @@
 # How to terminate 9 EC2 instances and leave one EC2 machine running.
 
+resource "aws_instance" "example" {
+  count         = 1  # Change from 10 to 1
+  ami           = "ami-12345678"
+  instance_type = "t2.micro"
+  tags = {
+    Name = "Instance-0"
+  }
+}
