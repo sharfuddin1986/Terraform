c3caeadfb6598598d7f82e37ee52e67d57f2a88f
Md Sharfuddin <106474548+sharfuddin1986@users.noreply.github.com>
2024-11-28 11:41:41 +0300

    Update terminate.tf

--- terminate.tf
+++ terminate.tf
@@ -1,5 +1,7 @@
 # How to terminate 9 EC2 instances and leave one EC2 machine running.
 
+Solution- write terraform file 
+
 resource "aws_instance" "example" {
   count         = 1  # Change from 10 to 1
   ami           = "ami-12345678"
